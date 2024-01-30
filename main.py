e7a29b58399babe8bc7d3e50c75548edfae14f3f
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-30 21:57:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from views.hello import hello_blueprint
 from views.url import url_blueprint
+from views.audio_player import audio_player_blueprint
 from flask_bootstrap import Bootstrap5
 from dotenv import load_dotenv
 import os
@@ -11,8 +11,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 bootstrap = Bootstrap5(app)
 
-app.register_blueprint(hello_blueprint)
 app.register_blueprint(url_blueprint)
+app.register_blueprint(audio_player_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
