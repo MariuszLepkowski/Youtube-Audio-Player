7c55edff856b02e3bfeba4fa745e239326ec252e
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-30 14:46:31 +0100

    Update hello.py

--- views/hello.py
+++ views/hello.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 hello_blueprint = Blueprint('hello', __name__)
 
 
-@hello_blueprint.route('/')
+@hello_blueprint.route('/hello')
 def hello_world():
     return 'Hello World!'
