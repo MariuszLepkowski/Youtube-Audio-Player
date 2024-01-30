1cb985807d1dd8fd18000caf2fd97564cd15c4a2
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-30 14:47:01 +0100

    Update project configuration inside main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from views.hello import hello_blueprint
+from views.url import url_blueprint
+from flask_bootstrap import Bootstrap5
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+bootstrap = Bootstrap5(app)
+
 app.register_blueprint(hello_blueprint)
+app.register_blueprint(url_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
