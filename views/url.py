6f5134b34e986867bb1d7189afa7dd22fa9ca63b
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-30 18:28:52 +0100

    Configure form submission

--- views/url.py
+++ views/url.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 from forms import UrlForm
 
 url_blueprint = Blueprint('url', __name__)
 
 
-@url_blueprint.route('/')
+@url_blueprint.route('/', methods=['GET', 'POST'])
 def enter_url():
     form=UrlForm()
+    if form.validate_on_submit():
+        return 'success'
+
     return render_template('url.html', form=form)
